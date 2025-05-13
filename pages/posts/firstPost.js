2fdf18d54b14878beb3240d930e9fdf4ff132b34
hcstnb2047 <yts2019hc2047@gmail.com>
2025-05-14 05:42:11 +0900

    initial commit

---
+++ pages/posts/firstPost.js
@@ -0,0 +1,12 @@
+
+import Link from 'next/link';
+
+export default function FirstPost() {
+    return <div>
+        <head>
+            <title>最初の投稿</title>
+        </head>
+        <h1>First Post</h1>
+        <h2>ホームへ戻る</h2>
+    </div>
+}
